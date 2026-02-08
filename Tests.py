5d1ef112b4c1c7d48f3dacbda76d5f2488d35353
Oldmemory1 <3558066700@qq.com>
2026-02-08 17:20:52 +0800

    Step07_1：测试反向传播自动化

--- Tests.py
+++ Tests.py
@@ -60,3 +60,17 @@ class Tests(unittest.TestCase):
         a.grad = B.backward(gy=b.grad)
         x.grad = A.backward(gy=a.grad)
         print(x.grad)
+    def test_step_07_1(self):
+        A = Square()
+        B = Exp()
+        C = Square()
+        x = Variable(np.array(0.5))
+        a = A(x)
+        b = B(a)
+        y = C(b)
+        assert y.creator == C
+        assert y.creator.input == b
+        assert y.creator.input.creator == B
+        assert y.creator.input.creator.input == a
+        assert y.creator.input.creator.input.creator == A
+        assert y.creator.input.creator.input.creator.input == x
--- Utility.py
+++ Utility.py
@@ -6,13 +6,19 @@ class Variable:
     def __init__(self,data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self,func):
+        self.creator = func
 
 class Function:
     def __call__(self,input:Variable):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
 
     def forward(self,x):
