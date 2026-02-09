cf23faf4ffc741b61092f7fd2de96870b4432aa3
Oldmemory1 <3558066700@qq.com>
2026-02-09 17:46:52 +0800

    step22_3：实现Variable的除法运算

--- Utility.py
+++ Utility.py
@@ -114,6 +114,12 @@ class Variable:
     def __rsub__(self, other):
         return sub(self,other)
 
+    def __truediv__(self, other):
+        return div(self,other)
+
+    def __rtruediv__(self, other):
+        return div(self,other)
+
 class Function:
     def __call__(self,*inputs):
         inputs = [as_variable(obj=input_) for input_ in inputs]
@@ -210,6 +216,26 @@ def sub(x0,x1):
 def rsub(x0,x1):
     x1 = as_array(x1)
     return Sub()(x1,x0)
+
+class Div(Function):
+    @override
+    def forward(self,x0,x1):
+        y = x0 / x1
+        return y
+    @override
+    def backward(self,gy):
+        x0 = self.inputs[0].data
+        x1 = self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+def div(x0,x1):
+    x1 = as_array(x1)
+    return Div()(x0,x1)
+def rdiv(x0,x1):
+    x1 = as_array(x1)
+    return Div()(x1,x0)
+
 
 def numerical_diff(f,x:Variable,eps = 1e-4):
     x0 = Variable(x.data-eps)
