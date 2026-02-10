0a4abfaf7708bedd959fac759e62f1acb2cc9617
Oldmemory1 <3558066700@qq.com>
2026-02-10 10:28:51 +0800

    step24_1：sphere

--- Tests_1.py
+++ Tests_1.py
@@ -9,6 +9,10 @@ if '__file__' in globals():
 
 from dezero import Variable
 
+def sphere(x,y):
+    z = x**2 + y**2
+    return z
+
 class Tests_1(unittest.TestCase):
     def test_step_23_1(self):
         x = Variable(np.array(1.0))
@@ -16,3 +20,11 @@ class Tests_1(unittest.TestCase):
         y.backward()
         print(y)
         print(x.grad)
+    def test_step_24_1(self):
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = sphere(x, y)
+        z.backward()
+        print(x.grad,y.grad)
+
+
