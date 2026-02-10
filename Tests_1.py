a8fd0a36f8dd5a37a9a6d1e16b635845e70bc9af
Oldmemory1 <3558066700@qq.com>
2026-02-10 10:30:21 +0800

    step24_2：matyas

--- Tests_1.py
+++ Tests_1.py
@@ -12,6 +12,9 @@ from dezero import Variable
 def sphere(x,y):
     z = x**2 + y**2
     return z
+def matyas(x,y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
 
 class Tests_1(unittest.TestCase):
     def test_step_23_1(self):
@@ -26,5 +29,12 @@ class Tests_1(unittest.TestCase):
         z = sphere(x, y)
         z.backward()
         print(x.grad,y.grad)
+    def test_step_24_2(self):
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = matyas(x, y)
+        z.backward()
+        print(x.grad,y.grad)
+
 
 
