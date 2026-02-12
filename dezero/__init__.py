ee6c0207f36c4d6c168c393422cee5c2b56d0d37
Oldmemory1 <3558066700@qq.com>
2026-02-12 13:36:59 +0800

    Step32_1：实现高阶导数

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,4 +1,6 @@
-simple_core = True
+simple_core = False
 
 if simple_core:
-    from dezero.simple_core import Variable, Function, Square, Exp, numerical_diff, square, exp, Add, add, mul
+    from dezero.simple_core import Variable, Function, Square, Exp, numerical_diff, square, exp, Add, add, mul, no_grad
+else:
+    from dezero.core import Variable, Function, Square, Exp, Add, add, mul, Mul, using_config, no_grad
